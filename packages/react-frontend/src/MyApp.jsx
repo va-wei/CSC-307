3fd3bd75de9576e5a0e5e017728aa505028b7579
va-wei <vawei@calpoly.edu>
2024-04-09 11:41:05 -0700

    step 9 commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,14 +1,10 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor" // the rest of the data
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -23,6 +19,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
