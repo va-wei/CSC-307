4cd0183b6b4310254dcf2a5b7811a33007e84293
va-wei <vawei@calpoly.edu>
2024-04-14 16:46:44 -0700

    added using post method to add users

--- packages/backend.js
+++ packages/backend.js
@@ -44,6 +44,17 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
