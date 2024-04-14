5cc3d065a0b9b3486059447ecb39706f958ed5d4
va-wei <vawei@calpoly.edu>
2024-04-14 14:56:13 -0700

    added users

--- packages/backend.js
+++ packages/backend.js
@@ -5,8 +5,38 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
@@ -14,3 +44,4 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
