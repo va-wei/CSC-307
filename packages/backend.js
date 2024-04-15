94137debb3895856ae0766ab9c5cac55328faee6
va-wei <vawei@calpoly.edu>
2024-04-14 17:22:06 -0700

    added delete by id and find users and job

--- packages/backend.js
+++ packages/backend.js
@@ -41,6 +41,13 @@ const findUserByName = (name) => {
   );
 };
 
+const findUsersByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
+
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -65,16 +72,34 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  let result = findUserById(id);
+  if (result === undefined) { // if the user DNE
+    res.status(404).send("Resource not found.");
+  } else { 
+    const index = users["users_list"].indexOf(result); // get index of user
+    users["users_list"].splice(index, 1); // remove from array
+    res.send(`User with id ${id} has been deleted`);
   }
 });
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+
+  // filter by both name and job if given
+  if (name != undefined && job != undefined) {
+    const filteredUsers = findUsersByNameAndJob(name, job);
+    res.send({ users_list: filteredUsers });
+  } else if (name != undefined) { // name only
+    const filteredUsers = findUserByName(name);
+    res.send({ users_list: filteredUsers });
+  } else {
+    res.send(users); // all users if no filter given
+  }
+});
+
 
 app.listen(port, () => {
   console.log(
